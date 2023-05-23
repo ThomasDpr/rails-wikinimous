faaadcc66d06c3155129fb21158df55897b88410
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-05-23 17:02:47 +0200

    not finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  # get 'articles/new'
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/create'
+  # get 'articles/destroy'
+  # get 'articles/edit'
+  # get 'articles/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles, only: [:create, :index, :new, :edit, :update, :show, :destroy]
 end
